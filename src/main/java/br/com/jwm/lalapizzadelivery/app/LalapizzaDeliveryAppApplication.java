5ca7821aa7637e3f083cb49bdef1eb5468a1afd3
Milena <milena_gr@hotmail.com>
2018-05-23 21:30:44 -0300

    inicio de projeto

---
+++ src/main/java/br/com/jwm/lalapizzadelivery/app/LalapizzaDeliveryAppApplication.java
@@ -0,0 +1,12 @@
+package br.com.jwm.lalapizzadelivery.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalapizzaDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalapizzaDeliveryAppApplication.class, args);
+	}
+}
